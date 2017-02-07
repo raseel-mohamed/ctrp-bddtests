4a900baf667f6fad7a7810f1c7378586dd6707a3
SHAMIM AHMED <shamim.ahmed@nih.gov>
2017-02-07 17:42:39 -0500

    updated env.rb document

--- ctrpBackend/features/support/env.rb
+++ ctrpBackend/features/support/env.rb
@@ -8,6 +8,7 @@ include Test::Unit::Assertions
 
 base_qa2 = 'https://trials-qa2.nci.nih.gov/'
 base_local = 'https://localhost/'
+base_aws = 'http://ctrp-external-alb-1048354744.us-east-1.elb.amazonaws.com:39080/'
 
 
 po_point = 'po-webservices/services/'
@@ -15,7 +16,7 @@ org_endpoint = 'organization-rest-service/organization/'
 per_endpoint = 'person-rest-service/person/'
 fam_endpoint = 'family-rest-service/'
 
-ENV['choose_ENV'] = 'local'
+ENV['choose_ENV'] = 'aws'
 ENV['user1'] = 'ctrpqatester1'
 ENV['user1_password'] = 'Welcome@1'
 
@@ -32,6 +33,12 @@ case ENV['choose_ENV']
     ENV['create_person'] = base_local + po_point + per_endpoint
     ENV['update_person'] = base_local + po_point + per_endpoint
     ENV['search_family'] = base_local + po_point + fam_endpoint
+  when 'aws'
+    ENV['create_organization'] = base_aws + po_point + org_endpoint
+    ENV['update_organization'] = base_aws + po_point + org_endpoint
+    ENV['create_person'] = base_aws + po_point + per_endpoint
+    ENV['update_person'] = base_aws + po_point + per_endpoint
+    ENV['search_family'] = base_aws + po_point + fam_endpoint
   else
     puts 'Please choose correct Environment.'
 end
