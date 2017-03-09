e4e83455bc50f2e1199e1a939b9a2065642eb752
SHAMIM AHMED <shamim.ahmed@nih.gov>
2017-03-09 13:24:01 -0500

    updated framework

--- ctrpUI/features_predefine/step_definitions/gmail_login.rb
+++ ctrpUI/features_predefine/step_definitions/gmail_login.rb
@@ -4,14 +4,14 @@ require 'selenium-cucumber'
 # Add your custom steps here
 # $driver is instance of webdriver use this instance to write your custom code
 
-Given(/^I navigate to "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-When(/^I wait for (\d+) sec$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
-
-Then(/^I close browser$/) do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+# Given(/^I navigate to "([^"]*)"$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# When(/^I wait for (\d+) sec$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
+#
+# Then(/^I close browser$/) do
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
