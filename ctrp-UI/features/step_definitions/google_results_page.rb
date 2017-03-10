259af185722a0ffaac0a4b20c7e482c02925e2db
SHAMIM AHMED <shamim.ahmed@nih.gov>
2017-03-10 12:45:03 -0500

    updated

--- ctrp-UI/features/step_definitions/google_results_page.rb
+++
@@ -1,3 +0,0 @@
-Then /^I should see the results page$/ do
-  @google_results_page.verify_page
-end
--- ctrp-UI/test_features/support/env.rb
+++
@@ -1,7 +0,0 @@
-require 'cucumber'
-require 'capybara/cucumber'
-#require ‘capybara’
-#require ‘capybara/rails’
-#require ‘capybara/dsl’
-require 'selenium-webdriver'
-require 'rspec/expectations'
