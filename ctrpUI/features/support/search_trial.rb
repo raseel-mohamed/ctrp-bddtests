30ad6730c0b409633d1c480b4f6053a14273fcfe
nalluruvn <vasu.nalluru@nih.gov>
2017-03-15 14:10:28 -0400

    Updating CTRPMICRO 16 step definitions.

---
+++ ctrpUI/features/support/search_trial.rb
@@ -0,0 +1,19 @@
+class SearchTrial
+
+  @trial_search_text_id = 'identifier'
+  @trial_search_button_id = 'search'
+  @trial_protocol_link_id = 'NCI-2013-00038'
+
+  def self.trial_search_text_id
+    @trial_search_text_id
+  end
+
+  def self.trial_search_button_id
+    @trial_search_button_id
+  end
+
+  def self.trial_protocol_link_id
+    @trial_protocol_link_id
+  end
+
+end
