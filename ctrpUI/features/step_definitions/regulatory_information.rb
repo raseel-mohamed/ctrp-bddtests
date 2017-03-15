9f5a306623f55dcefac539382047948e27cc5f93
nalluruvn <vasu.nalluru@nih.gov>
2017-03-15 18:44:57 -0400

    Updating CTRPMICRO 16 step definitions.

--- ctrpUI/features/step_definitions/regulatory_information.rb
+++ ctrpUI/features/step_definitions/regulatory_information.rb
@@ -2,7 +2,9 @@ require_relative '../support/home_page_obj'
 require_relative '../support/left_menu_navigation_obj'
 require 'selenium-cucumber'
 
-#@CTRPMICRO-16
+
+#@CTRPMICRO-16 & @CTRPMICRO-65
+#
 
 Given(/^I login into CTRP and search for a trial with NCI ID "([^"]*)"$/) do |arg1|
   step %[I navigate to "#{ENV['PA_APP']}"]
@@ -11,13 +13,31 @@ Given(/^I login into CTRP and search for a trial with NCI ID "([^"]*)"$/) do |ar
   step %[I click on element having id "#{HomePagePA.login_id}"]
   step %[I click on element having id "#{HomePagePA.accept_disclaimer_id}"]
   step %[I click on element having id "#{LeftMenuNavigation.search_trial_menu_id}"]
-  step %[I wait for 10 sec]
+  step %[I enter "#{arg1}" into input field having id "#{SearchTrial.trial_search_text_id}"]
+  step %[I click on element having class "#{SearchTrial.trial_search_button_id}"]
+  step %[I click on link having text "#{arg1}"]
+
+  step %[I wait for 2 sec]
+
 end
 
+# Regulatory Information link in the left menu. Xpath was used because the tag does not have an ID assigned.
 Given(/^I navigate to Regulatory Information screen$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+   step %[I click on element having xpath ".//*[@id='part_sites']/li[1]/a"]
 end
 
 Then(/^"([^"]*)" field is not displayed in Regulatory Information\(PA\) screen$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+  step %[element having xpath ".//*[@id='countries']" should not be present]
+  puts "#{arg1}"  "is not present in Regulatory Information screen."
 end
+
+# @CTRPMICRO-66
+
+Then(/^new fields with options should be there$/) do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  step %[element having id "drug" should be present]
+  puts "drug is present in the RI"
+end
+
+# @CTRPMICRO-75
+
