184d9124ae48e8bb6f708df01bd6f2999c05b807
nalluruvn <vasu.nalluru@nih.gov>
2017-03-16 23:05:46 -0400

    Updating CTRPMICRO 16 step definitions.

--- ctrpUI/features/support/regulatory_information_obj.rb
+++ ctrpUI/features/support/regulatory_information_obj.rb
@@ -1,6 +1,8 @@
 class RegulatoryInformation
   @drug_approval_dropdown_id = 'approval'
   @unapproved_uncleared_device_id = 'delpostindid'
+  @delayed_posting_indicator_id = 'delpostindid'
+
 
   def self.drug_approval_dropdown_id
     @drug_approval_dropdown_id
@@ -8,6 +10,10 @@ class RegulatoryInformation
 
   def self.unapproved_uncleared_device_id
     @unapproved_uncleared_device_id
+  end
+
+  def self.delayed_posting_indicator_id
+    @delayed_posting_indicator_id
   end
 
 end
