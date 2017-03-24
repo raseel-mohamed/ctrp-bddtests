174041e62eddd3ecca7bbbb0e8d4fd7ed3bac6ad
kadiyalas3 <sindhukadiyala.kadiyala@nih.gov>
2017-03-24 16:18:20 -0400

    Updated trial_ind_ide

--- ctrpUI/features/step_definitions/trial_ind_ide.rb
+++ ctrpUI/features/step_definitions/trial_ind_ide.rb
@@ -6,21 +6,22 @@ require 'selenium-cucumber'
 And(/^I navigate to IND\/IDE screen$/) do
   step %[I click on element having xpath "//a[text()='Trial IND/IDE']"]
 end
+
 #@pa_high @FDAAA @pa @CTRPMICRO-29
 When(/^I click on Add button$/) do
   step %[I click on element having xpath "#{TrialINDIDE.add_btn_xpath}"]
 end
 
 Then(/^Availability of Expanded Access should display a dropdown "([^"]*)" "([^"]*)" and "([^"]*)"$/) do |arg1,arg2,arg3|
-  step %[element having xpath "#{TrialINDIDE.ava_of_Exp_Acc_yes_xpath}" should have text as "#{arg1}"]
-  step %[element having xpath "#{TrialINDIDE.ava_of_Exp_Acc_no_xpath}" should have text as "#{arg2}"]
-  step %[element having xpath "#{TrialINDIDE.ava_of_Exp_Acc_unknown_xpath}" should have text as "#{arg3}"]
+  step %[element having xpath "#{TrialINDIDE.ava_of_exp_acc_yes_xpath}" should have text as "#{arg1}"]
+  step %[element having xpath "#{TrialINDIDE.ava_of_exp_acc_no_xpath}" should have text as "#{arg2}"]
+  step %[element having xpath "#{TrialINDIDE.ava_of_exp_acc_unknown_xpath}" should have text as "#{arg3}"]
 
 end
 
 #@pa_high @FDAAA @pa @CTRPMICRO-68
 
 Then(/^"([^"]*)" label is displayed$/) do |arg|
-  expect(get_element_text("xpath","#{TrialINDIDE.ava_of_Exp_Acc_xpath}").split("*").first.strip.chomp(":")).to eq(arg)
+  expect(get_element_text("xpath","#{TrialINDIDE.ava_of_exp_acc_xpath}").split("*").first.strip.chomp(":")).to eq(arg)
 end
 
