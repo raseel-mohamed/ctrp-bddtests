880c062d2b5856d1b8bcd13c5a43f486b087e80e
shilpi85 <shilpi.rauniyar@gmail.com>
2017-03-30 16:56:16 -0400

    removing information from env

--- ctrpBackend/features/support/env.rb
+++ ctrpBackend/features/support/env.rb
@@ -31,11 +31,7 @@ import_endpoint = 'abbreviated/'
 #PA Endpoints
 
 
-ENV['choose_ENV'] = 'local'
-ENV['user1'] =  'ctrpqatester1'
-ENV['user1_password'] = 'pass'
-ENV['user2'] =  'ctrpsubstractor'
-ENV['user2_password'] = 'pass'
+ENV['choose_ENV'] = 'aws'
 ENV['dct_usr'] = ''
 ENV['dct_pass'] = ''
 ENV['ecmms_usr'] = ''
@@ -60,8 +56,6 @@ case ENV['choose_ENV']
     ENV['db_hostname'] = 'localhost'
     ENV['db_port'] = '5432'
     ENV['db_name'] = 'padb'
-    ENV['db_user'] = 'postgres'
-    ENV['db_pass'] = ''
     ENV['create_organization'] = base_local + po_endpoint + org_endpoint
     ENV['update_organization'] = base_local + po_endpoint + org_endpoint
     ENV['create_person'] = base_local + po_endpoint + per_endpoint
@@ -75,8 +69,6 @@ case ENV['choose_ENV']
     ENV['db_hostname'] = 'ctrp.clb9vkosemwm.us-east-1.rds.amazonaws.com'
     ENV['db_port'] = '5432'
     ENV['db_name'] = 'pa_ctrpn'
-    ENV['db_user'] = 'pauser'
-    ENV['db_pass'] = 'pauser'
     ENV['create_organization'] = base_aws + po_endpoint + org_endpoint
     ENV['update_organization'] = base_aws + po_endpoint + org_endpoint
     ENV['create_person'] = base_aws + po_endpoint + per_endpoint
